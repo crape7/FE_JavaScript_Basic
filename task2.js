7522ba52ecaa334da2931a509ac1d2f01e107c42
yun <jan2218@naver.com>
2023-03-31 20:19:35 +0900

    문제2번

--- task2.js
+++ task2.js
@@ -1,1 +1,11 @@
 /* 2번 문제 답안을 작성해주세요. */
+function solution(num_list) {
+    let odd = 0;
+    for(let i = 0; i<num_list.length; i++) {
+        if (num_list[i]%2===1) {
+            odd++;
+        }
+    }
+    let answer = [num_list.length-odd,odd];
+    return answer;
+}
